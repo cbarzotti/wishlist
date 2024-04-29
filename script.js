ed289d00aeca40978c4831dffe868fdfe428a5dd
cbarzotti <barzotticristian@hotmail.it>
2024-04-29 20:40:22 +0200

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const checkboxes = document.querySelectorAll('.checkbox');
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', function() {
+            const isChecked = this.checked;
+            const itemId = this.id;
+
+            // Salvataggio dello stato della checkbox nello localStorage
+            localStorage.setItem(itemId, isChecked);
+        });
+
+        // Caricamento dello stato della checkbox dal localStorage
+        const isChecked = localStorage.getItem(checkbox.id);
+        if (isChecked === 'true') {
+            checkbox.checked = true;
+        }
+    });
+});
