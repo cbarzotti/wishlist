6efdc057e33bb2e76b8a876bb1624c0366427f29
cbarzotti <barzotticristian@hotmail.it>
2024-04-29 21:12:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const checkboxes = document.querySelectorAll('.checkbox');
 
     checkboxes.forEach(checkbox => {
-        console.log(checkbox.id);
+        
         checkbox.addEventListener('change', function() {
             const isChecked = this.checked;
             const itemId = this.id;
@@ -11,12 +11,21 @@ document.addEventListener("DOMContentLoaded", function() {
             localStorage.setItem(itemId, isChecked);
         });
 
-        // Caricamento dello stato della checkbox dal localStorage
-        const isChecked = localStorage.getItem(checkbox.id);
-        if (isChecked === 'true') {
-            checkbox.checked = true;
-        }
+
     });
+
+    function refreshState() {
+        checkboxes.forEach(checkbox => {
+            
+            const isChecked = localStorage.getItem(checkbox.id);
+            console.log(checkbox, isChecked);
+            if (isChecked === 'true') {
+                checkbox.checked = true;
+            }
+        })
+    }
+
+    setInterval(refreshState, 10000);
 });
 
 
